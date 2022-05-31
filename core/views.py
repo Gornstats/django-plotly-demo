e69f294e22791406d294e5f4cdaf0c2d01347ef6
Jeremy Gorniak <railspider@gmail.com>
2022-05-31 22:57:13 +1000

    Updated plotly chart styling

--- core/views.py
+++ core/views.py
@@ -9,7 +9,16 @@ def chart(request):
     fig = px.line(
         x = [c.date for c in co2],
         y = [c.average for c in co2],
+        title = 'CO2 Emissions (Parts per million)',
+        labels={'x': 'Date', 'y': 'PPM'},
     )
+    
+    fig.update_layout(title={
+        'font_size': 24,
+        'xanchor': 'center',
+        'x': 0.5,
+    })
+    
     chart = fig.to_html()
     context = {'chart': chart}
     
